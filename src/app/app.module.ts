e74e0cda805865e6f6b359197200dd74fb0deb29
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-11-04 14:35:40 +0530

    view data component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewDataComponent } from './view-data/view-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewDataComponent
   ],
   imports: [
     BrowserModule,

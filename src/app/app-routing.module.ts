bad98391f506fb99fbffbc82fdeba684be5e58ee
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-11-04 14:52:04 +0530

    routing path created for flipkart amazon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlipkartComponent } from './flipkart/flipkart.component';
+import { AmazonComponent } from './amazon/amazon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "flipkart",
+    component: FlipkartComponent
+  },
+  {
+    path: "amazon",
+    component: AmazonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

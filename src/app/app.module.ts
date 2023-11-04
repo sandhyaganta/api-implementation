3fcac745c9a75dd2956a9cae5fbdb5f5f43cf726
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-11-04 15:05:39 +0530

    http protocall added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ViewDataComponent } from './view-data/view-data.component';
 import { FlipkartComponent } from './flipkart/flipkart.component';
 import { AmazonComponent } from './amazon/amazon.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AmazonComponent } from './amazon/amazon.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
